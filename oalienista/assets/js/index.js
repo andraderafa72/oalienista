1292155c0ccee488fbe728c5ddfb8eb2099fd2ee
Rafael Andrade <devrafael72@gmail.com>
2021-10-16 19:47:34 -0300

    Correção em botão de saiba mais

---
+++ oalienista/assets/js/index.js
@@ -0,0 +1,22 @@
+const burgerToggle = document.querySelector('.hamburger');
+const burgerMenu = document.querySelector('.burger-menu');
+const header = document.querySelector('body > header');
+
+burgerToggle.addEventListener('click', e =>{
+  if(burgerToggle.classList.contains('is-active')){
+    burgerToggle.classList.remove('is-active');
+    burgerMenu.classList.remove('visible');
+  } else {
+    burgerToggle.classList.add('is-active');
+    burgerMenu.classList.add('visible');
+    header.classList.remove('shadow')
+  }
+})
+
+window.addEventListener('scroll', e => {
+  if(window.pageYOffset >= 10 && !burgerToggle.classList.contains('is-active')){ 
+    header.classList.add('shadow')
+  } else {
+    header.classList.remove('shadow')
+  }
+});
